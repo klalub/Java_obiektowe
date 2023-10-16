70f54c0241adeed76a6cdb4dc0f4036b721d12ea
klalub <115089005+klalub@users.noreply.github.com>
2023-10-16 11:57:35 +0200

    Create Class.java

---
+++ Class.java
@@ -0,0 +1,60 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//
+//        House domStefana = new House();
+//        domStefana.area = 150;
+//        domStefana.garage = true;
+//        domStefana.rooms = 6;
+//        domStefana.garden = false;
+//        domStefana.floors = 2;
+//        System.out.println(domStefana.getPrice());
+//
+//        House domSylwii = new House();
+//        domSylwii.area = 175;
+//        domSylwii.garage = false;
+//        domSylwii.rooms = 4;
+//        domSylwii.garden = true;
+//        domSylwii.floors = 2;
+//        System.out.println(domSylwii.getPrice());
+//
+//        Pies a = new Pies();
+//        a.nazwa = "Rex";
+//        a.rasa = "Husky";
+//        a.wiek = 6;
+//        a.dajGlos();
+//
+//        Pies b = new Pies();
+//        b.nazwa = "Kredo";
+//        b.rasa = "Doberman";
+//        b.wiek = 13;
+//        b.dajGlos();
+//
+//        Samochod a = new Samochod();
+//        a.marka = "Audi";
+//        a.model = "A6";
+//        a.predkosc = 60;
+//        System.out.println(a.przyspiesz(40));
+//        System.out.println(a.zwolnij(120));
+//
+//
+//        Samochod b = new Samochod();
+//        b.marka = "Citroen";
+//        b.model = "C4 Picasso";
+//        b.predkosc = 30;
+//        System.out.println(b.zwolnij(120));
+//        System.out.println(b.przyspiesz(40));
+
+        KontoBankowe a = new KontoBankowe();
+        a.saldo = 50000;
+        a.wplac(1000);
+        System.out.println(a.saldo);
+        a.wyplac(6000000);
+        
+
+
+
+
+    }
+}
